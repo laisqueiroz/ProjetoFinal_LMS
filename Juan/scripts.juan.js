bf0562fc9c765b4da89f0c59d8b449fbbf3505c0
Juandbpimentel <juandbpimentel@alu.ufc.br>
2023-11-26 20:59:17 -0300

    site pronto

--- Juan/scripts.juan.js
+++ Juan/scripts.juan.js
@@ -1,8 +1,100 @@
-const tituloElement = document.querySelector('.container_titulo');
+window.onload = async function () {
+	let section = document.querySelector('section')
+	let spinnerContainer = document.querySelector('#spinnerContainer')
 
-const buttonElement = document.querySelector('#botaoTeste');
+	let vetorDePokemons = []
+	let vetorDeElementos = []
 
-buttonElement.addEventListener('click', (event)=>{
-    event.preventDefault();
-    tituloElement.classList.toggle('active');
+	await fetchPokemons(vetorDePokemons)
+	generateElementos(vetorDePokemons, vetorDeElementos)
+
+	vetorDeElementos.forEach((card) => {
+		section.appendChild(card)
+	})
+	spinnerContainer.removeChild(spinnerContainer.lastElementChild)
+}
+
+async function fetchPokemons(vetorDePokemons) {
+	while (vetorDePokemons.length < 3) {
+		console.log(vetorDePokemons.length)
+		let numeroAleatorio = Math.floor(Math.random() * 1290 + 1)
+		if (numeroAleatorio > 1017) numeroAleatorio = numeroAleatorio - 1017 + 10000
+
+		let resultado = vetorDePokemons.filter((pokemon) => {
+			if (pokemon.id == numeroAleatorio) return true
+			return false
+		})
+		if (!resultado.includes(true)) {
+			let novoPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`)
+			vetorDePokemons.push(await novoPokemon.json())
+		}
+	}
+}
+
+function generateElementos(vetorDePokemons, vetorDeElementos) {
+	for (let i = 0; i < 3; i++) {
+		let card = document.createElement('div')
+		card.classList.add('card')
+		card.style.width = '25%'
+
+		let cardImg = document.createElement('img')
+		cardImg.classList.add('card-img-top')
+		cardImg.src = vetorDePokemons[i].sprites.other['official-artwork'].front_default
+		cardImg.style.width = 'auto'
+
+		let cardBody = document.createElement('div')
+		cardBody.classList.add('card-body')
+
+		let cardTitle = document.createElement('h4')
+		cardTitle.classList.add('card-title')
+		let nomeDoPokemon = ''
+		let vetorNomePokemon = vetorDePokemons[i].name.split('-')
+
+		vetorNomePokemon.forEach((palavra) => {
+			const capitalized = palavra.charAt(0).toUpperCase() + palavra.slice(1)
+			nomeDoPokemon += capitalized + ' '
+		})
+		cardTitle.innerHTML = nomeDoPokemon
+
+		let cardText = document.createElement('p')
+		cardText.innerHTML = '<b>Id do pokemon:</b> ' + vetorDePokemons[i].id + '<br><b>Altura:</b> ' + vetorDePokemons[i].height + '<br><b>Peso:</b> ' + vetorDePokemons[i].weight
+
+		cardBody.appendChild(cardTitle)
+		cardBody.appendChild(cardText)
+		card.appendChild(cardImg)
+		card.appendChild(cardBody)
+
+		vetorDeElementos.push(card)
+	}
+}
+
+function generateSpinner() {
+	let spinnerContainer = document.querySelector('#spinnerContainer')
+	spinnerContainer.innerHTML = `<div class="text-center" id="spinner">
+    <div class="spinner-border" role="status">
+    </div>
+    </div>`
+}
+
+function removeSpinner() {
+    let spinnerContainer = document.querySelector('#spinnerContainer')
+    spinnerContainer.removeChild(spinnerContainer.lastElementChild)
+}
+
+let botaoGerarPokemons = document.querySelector('#botaoGerarPokemons')
+
+botaoGerarPokemons.addEventListener('click', async (event) => {
+	generateSpinner()
+    let elemento = document.querySelector('section')
+	let vetorDePokemons = []
+	let vetorDeElementos = []
+
+	while (elemento.firstChild) elemento.removeChild(elemento.firstChild)
+
+	await fetchPokemons(vetorDePokemons)
+	generateElementos(vetorDePokemons, vetorDeElementos)
+	vetorDeElementos.forEach((card) => {
+		elemento.appendChild(card)
+	})
+    removeSpinner()
 })
